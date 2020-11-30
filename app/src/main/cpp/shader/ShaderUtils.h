af8d6f166e69f2c947dadda7e865681da6b8c305
chenhao <chenhao@wavewisdom.com>
2020-11-30 14:17:27 +0800

    增加shader里的异常判断

--- app/src/main/cpp/shader/ShaderUtils.h
+++ app/src/main/cpp/shader/ShaderUtils.h
@@ -13,22 +13,65 @@
 
 static GLuint loadShader(GLenum shaderType, const char *shaderSource) {
     int shader = glCreateShader(shaderType);
-    if (shader != 0) {
-        glShaderSource(shader, 1, &shaderSource, 0);
-        glCompileShader(shader);
+    if (shader == GL_NONE) {
+        LOGE("create shader failed! shaderType: %d", shaderType);
+        return GL_NONE;
+    }
+    glShaderSource(shader, 1, &shaderSource, 0);
+    glCompileShader(shader);
+    GLint compiled;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if (compiled == GL_FALSE) { // compile failed
+        GLint len = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+        if (len > 1) {
+            char *log = static_cast<char *>(malloc(sizeof(char) * len));
+            glGetShaderInfoLog(shader, len, NULL, log);
+            LOGE("Error compiling shader: %s", log);
+            free(log);
+        }
+        glDeleteShader(shader); // delete shader
+        return GL_NONE;
     }
     return shader;
 }
 
 static GLuint createProgram(const char *vertexSource, const char *fragmentSource) {
     int vertexShader = loadShader(GL_VERTEX_SHADER, vertexSource);
+    if (vertexShader == GL_NONE) {
+        LOGE("load vertex shader failed! ");
+        return GL_NONE;
+    }
     int fragmentShader = loadShader(GL_FRAGMENT_SHADER, fragmentSource);
+    if (fragmentShader == GL_NONE) {
+        LOGE("load fragment shader failed! ");
+        return GL_NONE;
+    }
     int program = glCreateProgram();
+    if (program == GL_NONE) {
+        LOGE("create program failed! ");
+        return GL_NONE;
+    }
     glAttachShader(program, vertexShader);
     glAttachShader(program, fragmentShader);
+    // 把着色器对象链接到程序对象以后，记得删除着色器对象，我们不再需要它们了
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     glLinkProgram(program);
+    GLint linked;
+    glGetProgramiv(program, GL_LINK_STATUS, &linked);
+    if (linked == GL_FALSE) { // link failed
+        GLint len = 0;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
+        if (len > 1) {
+            char *log = static_cast<char *>(malloc(sizeof(char) * len));
+            glGetProgramInfoLog(program, len, NULL, log);
+            LOGE("Error link program: %s", log);
+            free(log);
+        }
+        glDeleteProgram(program); // delete program
+        return GL_NONE;
+    }
     return program;
 }
 
--- app/src/main/java/com/apkcore/opengltriangle/java/ShaderUtils.java
+++ app/src/main/java/com/apkcore/opengltriangle/java/ShaderUtils.java
@@ -8,9 +8,20 @@ public class ShaderUtils {
 
     public static int loadShader(int shaderType, String shaderSource) {
         int shader = GLES30.glCreateShader(shaderType);
-        if (shader != 0) {
-            GLES30.glShaderSource(shader, shaderSource);
-            GLES30.glCompileShader(shader);
+        if (shader == GLES30.GL_NONE) {
+            Log.e(TAG, "create shared failed! type: " + shaderType);
+            return GLES30.GL_NONE;
+        }
+        GLES30.glShaderSource(shader, shaderSource);
+        GLES30.glCompileShader(shader);
+        // 4. check compile status
+        int[] compiled = new int[1];
+        GLES30.glGetShaderiv(shader, GLES30.GL_COMPILE_STATUS, compiled, 0);
+        if (compiled[0] == GLES30.GL_FALSE) { // compile failed
+            Log.e(TAG, "Error compiling shader. type: " + shaderType + ":");
+            Log.e(TAG, GLES30.glGetShaderInfoLog(shader));
+            GLES30.glDeleteShader(shader); // delete shader
+            shader = GLES30.GL_NONE;
         }
         return shader;
     }
@@ -21,13 +32,33 @@ public class ShaderUtils {
     //glDeleteShader(fragmentShader);
     public static int createProgram(String vertexSource, String fragmentSource) {
         int vertexShader = loadShader(GLES30.GL_VERTEX_SHADER, vertexSource);
+        if (vertexShader == GLES30.GL_NONE) {
+            Log.e(TAG, "load vertex shader failed! ");
+            return GLES30.GL_NONE;
+        }
         int fragmentShader = loadShader(GLES30.GL_FRAGMENT_SHADER, fragmentSource);
+        if (fragmentShader == GLES30.GL_NONE) {
+            Log.e(TAG, "load fragment shader failed! ");
+            return GLES30.GL_NONE;
+        }
         int program = GLES30.glCreateProgram();
+        if (program == GLES30.GL_NONE) {
+            Log.e(TAG, "create program failed! ");
+            return GLES30.GL_NONE;
+        }
         GLES30.glAttachShader(program, vertexShader);
         GLES30.glAttachShader(program, fragmentShader);
         GLES30.glDeleteShader(vertexShader);
         GLES30.glDeleteShader(fragmentShader);
         GLES30.glLinkProgram(program);
+        int[] linkStatus = new int[1];
+        GLES30.glGetProgramiv(program, GLES30.GL_LINK_STATUS, linkStatus, 0);
+        if (linkStatus[0] == GLES30.GL_FALSE) { // link failed
+            Log.e(TAG, "Error link program: ");
+            Log.e(TAG, GLES30.glGetProgramInfoLog(program));
+            GLES30.glDeleteProgram(program); // delete program
+            return GLES30.GL_NONE;
+        }
         return program;
     }
 }
